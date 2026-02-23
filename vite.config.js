91634444aadbf7f13801bce70c48261295eeeb70
Prajakta Magar <prajakta7magar@gmail.com>
2026-02-23 17:53:53 +0530

    making web app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Currency_Convertor/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Currency Convertor App',
+        short_name: 'currency convertor',
+        description: 'currency convertor app',
+        theme_color: '#8B4513',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/public/image192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/public/image512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
